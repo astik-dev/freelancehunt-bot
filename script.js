977c484a65784f516d8f0edcdbc1a45f0f130b81
ASTIK <astik.ytt@gmail.com>
2024-02-19 23:24:19 +0100

    Update project monitoring to handle pagination and date-based checking

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ import { config } from "./config.js";
 
 
 
-const lastProjectIdFile = "last-project-id.json";
-let lastProjectId = fs.existsSync(lastProjectIdFile) ? Number(fs.readFileSync(lastProjectIdFile, 'utf-8')) : undefined;
+const lastProjectTimeFile = "last-project-time.txt";
+let lastProjectTime = fs.existsSync(lastProjectTimeFile) ? new Date(fs.readFileSync(lastProjectTimeFile, 'utf-8')) : new Date();
 
 const bot = new TelegramBot(config.telegram.token, {polling: true});
 
@@ -17,6 +17,8 @@ function createProjectMessage(project) {
     message += project.attributes.budget ? "💵 "+project.attributes.budget.amount+" "+project.attributes.budget.currency+"\n" : "";
     message += "\n";
     message += project.attributes.description;
+    message += "\n\n";
+    message += project.attributes.published_at;
     return message;
 }
 
@@ -24,49 +26,86 @@ function timeLog(message) {
     console.log(`${new Date().toLocaleTimeString()} | ${message}`);
 }
 
-function fetchFreelancehunt() {
+async function getNewProjects(response, newProjects) {
 
-    timeLog("Fetch");
-    
-    fetch(`https://api.freelancehunt.com/v2/projects?filter[skill_id]=${config.freelancehunt.skillIds}`, {
+    if (!newProjects) newProjects = [];
+
+    const projects = response.data;
+    let nextPageNeeded = true;
+
+    projects.forEach(project => {
+        const currentProjectTime = new Date(project.attributes.published_at);
+        if (currentProjectTime > lastProjectTime) {
+            newProjects.push(project);
+        } else {
+            nextPageNeeded = false;
+        }
+    });
+
+    if (nextPageNeeded) {
+        await delay(1000);
+        const nextPageResponse = await fetchFreelancehuntJSON(response.links.next);
+        return getNewProjects(nextPageResponse, newProjects);
+    } else {
+        return newProjects;
+    }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchFreelancehuntJSON(link) {
+
+    const response = await fetch(link, {
         headers: {
             'Authorization': config.freelancehunt.token,
         }
-    })
-        .then(response => response.json())
-        .then(response => {
-            
-            const projects = response.data;
-            const newLastProjectId = response.data[0].id;
+    });
 
-            if (!lastProjectId)
-                lastProjectId = response.data[response.data.length - 1].id - 1;
+    return response.json();
+}
 
-            projects.forEach(project => {
+async function checkForNewProjects() {
+    
+    try {
+        timeLog("Fetch");
+        
+        const responseJSON = await fetchFreelancehuntJSON(`https://api.freelancehunt.com/v2/projects?filter[skill_id]=${config.freelancehunt.skillIds}`);
 
-                if (project.id > lastProjectId) {
-                    
-                    const message = createProjectMessage(project);
-                    
-                    bot.sendMessage(config.telegram.chatId, message, {
-                        parse_mode: 'HTML',
-                        disable_web_page_preview: true
-                    });
+        const newProjects = await getNewProjects(responseJSON);
 
-                    timeLog(`New project: ${project.attributes.name}`);
+        for (let i = newProjects.length - 1; i >= 0; i--) {
+
+            const project = newProjects[i];
+
+            bot.sendMessage(
+                config.telegram.chatId,
+                createProjectMessage(project),
+                {
+                    parse_mode: 'HTML',
+                    disable_web_page_preview: true
                 }
-            });
+            );
 
-            fs.writeFileSync(lastProjectIdFile, `${newLastProjectId}`);
-            lastProjectId = newLastProjectId;
-        })
-        .catch(error => timeLog(`Error: ${error}`));
+            timeLog(`New project: ${project.attributes.name}`);
+
+            await delay(1000);
+        }
+
+        const newLastProjectTime = responseJSON.data[0].attributes.published_at;
+        fs.writeFileSync(lastProjectTimeFile, `${newLastProjectTime}`);
+        lastProjectTime = new Date(newLastProjectTime);
+
+    } catch (error) {
+        timeLog(`Error: ${error}`);
+    }
 }
 
 
 
-fetchFreelancehunt();
+checkForNewProjects();
 
 setInterval(() => {
-    fetchFreelancehunt();
+    checkForNewProjects();
 }, 3 * 60 * 1000);
