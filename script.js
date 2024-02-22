30658ec1bd726867fd694da56c04ddc39f066036
ASTIK <astik.ytt@gmail.com>
2024-02-22 15:06:42 +0100

    Add bid analysis functionality

--- script.js
+++ script.js
@@ -117,6 +117,9 @@ async function checkForNewProjects() {
                             ],
                             [
                                 { text: `Add bid`, callback_data: `add_bid:${project.id},${project.attributes?.budget?.currency || "UAH"}` }
+                            ],
+                            [
+                                { text: `Bid Analysis`, callback_data: `bid_analysis:${project.id}` }
                             ]
                         ]
                     }
@@ -242,6 +245,77 @@ async function addBid(projectId, projectCurrency, projectMessageId) {
     canCheckForNewProjects = true;
 }
 
+function getAverage(numbers) {
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    return Math.round(sum / numbers.length);
+}
+
+function getMinMaxAvg(numbers) {
+    return {
+        min: Math.min(...numbers),
+        max: Math.max(...numbers),
+        avg: getAverage(numbers)
+    }
+}
+
+function generateOccurrencesString(numbers) {
+    // count occurrences
+    let occurrences = {};
+    numbers.forEach(number => {
+        occurrences[number] = (occurrences[number] || 0) + 1;
+    });
+    // sort occurrences
+    const sortedOccurrencesArray =  Object.entries(occurrences).sort((a, b) => b[1] - a[1]);
+    // occurrences array to string
+    return sortedOccurrencesArray.map(([num, count]) => `${num} (${count})`).join(', ');
+}
+
+async function bidAnalysis(projectId, projectMessageId) {
+    const responseJSON = await fetchFreelancehuntJSON(`https://api.freelancehunt.com/v2/projects/${projectId}/bids`);
+    const allBids = responseJSON.data;
+    const unhiddenBids = allBids.filter(bid => !bid.attributes.is_hidden);
+
+    let message =  "<b>📊 Bid analysis</b>\n";
+        message += `\n`;
+        message += `<b><i>Bids:</i></b> ${allBids.length}\n`;
+        message += `<b><i>Unhidden bids:</i></b> ${unhiddenBids.length}`;
+
+    if (allBids.length != 0 && unhiddenBids.length != 0) {
+        
+        const bidCurrency = unhiddenBids[0].attributes.budget.currency;
+
+        const bidAmounts = unhiddenBids.map(bid => bid.attributes.budget.amount);
+        const bidAmount = getMinMaxAvg(bidAmounts);
+        const bidAmountsOccurrences = generateOccurrencesString(bidAmounts);
+
+        const bidDays = unhiddenBids.map(bid => bid.attributes.days);
+        const projectDuration = getMinMaxAvg(bidDays);
+        const bidDaysOccurrences = generateOccurrencesString(bidDays);
+
+        message += `\n\n`;
+        message += `<b>💵 <u>Price</u></b>\n`
+        message += `<b>MAX:</b> ${bidAmount.max} ${bidCurrency}\n`;
+        message += `<b>AVG:</b> ${bidAmount.avg} ${bidCurrency}\n`;
+        message += `<b>MIN:</b> ${bidAmount.min} ${bidCurrency}\n`;
+        message += `<b>Occurrences:</b> ${bidAmountsOccurrences}\n`;
+        message += `\n`;
+        message += `<b>⌛ <u>Project duration</u></b>\n`
+        message += `<b>MAX:</b> ${projectDuration.max} days\n`;
+        message += `<b>AVG:</b> ${projectDuration.avg} days\n`;
+        message += `<b>MIN:</b> ${projectDuration.min} days\n`;
+        message += `<b>Occurrences:</b> ${bidDaysOccurrences}`;
+    }
+
+    bot.sendMessage(
+        config.telegram.chatId,
+        message,
+        {
+            parse_mode: 'HTML',
+            reply_to_message_id: projectMessageId,
+        }
+    );
+}
+
 
 
 bot.on("callback_query", (callbackQuery) => {
@@ -252,6 +326,9 @@ bot.on("callback_query", (callbackQuery) => {
     if (command == "add_bid") {
         const [projectId, projectCurrency] = data.split(',');
         addBid(projectId, projectCurrency, projectMessage);
+    } else if (command == "bid_analysis") {
+        const projectId = data;
+        bidAnalysis(projectId, projectMessage);
     }
 });
 
