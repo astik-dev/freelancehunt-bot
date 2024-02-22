895630ed3d9d162fafeaedf44f16378c15e64faa
ASTIK <astik.ytt@gmail.com>
2024-02-22 12:11:46 +0100

    Add bid functionality

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let lastProjectTime = fs.existsSync(lastProjectTimeFile) ? new Date(fs.readFileS
 
 const bot = new TelegramBot(config.telegram.token, {polling: true});
 
+let canCheckForNewProjects = true;
+
 
 
 function createProjectMessage(project) {
@@ -112,6 +114,9 @@ async function checkForNewProjects() {
                             ],
                             [
                                 { text: `Employer (${projectEmployerLogin})`, url: `https://freelancehunt.com/employer/${projectEmployerLogin}.html` }
+                            ],
+                            [
+                                { text: `Add bid`, callback_data: `add_bid:${project.id},${project.attributes?.budget?.currency || "UAH"}` }
                             ]
                         ]
                     }
@@ -132,10 +137,130 @@ async function checkForNewProjects() {
     }
 }
 
+function waitUserResponse() {
+    return new Promise(resolve => {
+        bot.once('message', (responseMessage) => {
+            resolve(responseMessage.text);
+        });
+    });
+}
+
+async function sendMessageAndWaitForResponse(message) {
+    await bot.sendMessage(config.telegram.chatId, `<b>${message}</b>`, { parse_mode: 'HTML' });
+    return await waitUserResponse();
+}
+
+async function addBid(projectId, projectCurrency, projectMessageId) {
+
+    canCheckForNewProjects = false;
+
+    const bid = {
+        "days": null,
+        "safe_type": "employer",
+        "budget": {
+            "amount": null,
+            "currency": projectCurrency
+        },
+        "comment": null,
+        "is_hidden": true
+    };
+
+    await bot.sendMessage(
+        config.telegram.chatId,
+        `📝 Adding a bid for the project:`,
+        {
+            disable_web_page_preview: true,
+            reply_to_message_id: projectMessageId,
+        }
+    );
+
+    bid.comment = await sendMessageAndWaitForResponse(`Send a commentary`);
+    bid.budget.amount = await sendMessageAndWaitForResponse(`Send a price (${projectCurrency})`);
+    bid.days = await sendMessageAndWaitForResponse(`Send a project duration`);
+
+    let bidMessage =  `<b>💬 Commentary:</b>\n<blockquote>${bid.comment}</blockquote>\n`;
+        bidMessage += `<b>💵 Price:</b> ${bid.budget.amount} ${projectCurrency}\n`;
+        bidMessage += `<b>🕔 Project duration:</b> ${bid.days} days`;
+
+    await bot.sendMessage(
+        config.telegram.chatId,
+        bidMessage,
+        {
+            disable_web_page_preview: true,
+            parse_mode: 'HTML',
+            reply_markup: {
+                keyboard: [
+                    ['❌ Cancel', '✅ Add'],
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: true,
+            },
+        }
+    );
+    const bidConfirmation = await sendMessageAndWaitForResponse("Confirm adding bid");
+    
+    let bidResultMessage;
+
+    if (bidConfirmation == "✅ Add") {
+        try {
+            const bidResponse = await fetch(`https://api.freelancehunt.com/v2/projects/${projectId}/bids`, {
+                method: 'POST',    
+                headers: {
+                    "Authorization": config.freelancehunt.token,
+                    "Content-Type": "application/json",
+                },
+                body: bid
+            });
+
+            if (!bidResponse.ok) {
+                throw new Error(`HTTP error! Status: ${bidResponse.status}`);
+            } else {
+                bidResultMessage = "✅ Bid successfully added";
+            }
+        } catch (error) {
+            timeLog(`Bid error: ${error}`);
+            bidResultMessage = `❌ Bid error: ${error}`;
+        }
+    } else {
+        if (bidConfirmation != "❌ Cancel") {
+            await bot.sendMessage(config.telegram.chatId, `<b>❌ Unknown command</b>`, { parse_mode: 'HTML' });
+        }
+        bidResultMessage = "❌ Bid addition canceled";
+    }
+
+    await bot.sendMessage(
+        config.telegram.chatId,
+        `<b>${bidResultMessage}</b>`, 
+        {
+            parse_mode: 'HTML',
+            reply_markup: {
+                remove_keyboard: true,
+            },
+        }
+    );
+
+    canCheckForNewProjects = true;
+}
+
+
+
+bot.on("callback_query", (callbackQuery) => {
+
+    const [command, data] = callbackQuery.data.split(':');
+    const projectMessage = callbackQuery.message.message_id;
+
+    if (command == "add_bid") {
+        const [projectId, projectCurrency] = data.split(',');
+        addBid(projectId, projectCurrency, projectMessage);
+    }
+});
+
 
 
 checkForNewProjects();
 
 setInterval(() => {
-    checkForNewProjects();
+    if (canCheckForNewProjects) {
+        checkForNewProjects();
+    }
 }, 3 * 60 * 1000);
